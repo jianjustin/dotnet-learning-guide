69136f4b6252060793b43d593961566cb746dabd
chenjianrui <chenjianrui@beisen.com>
2020-09-16 15:27:09 +0800

    添加数据库连接测试

--- UnitTestProject/MysqlConnectionTest.cs
+++ UnitTestProject/MysqlConnectionTest.cs
@@ -2,13 +2,21 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using UnitTestProject.MySql;
 
 namespace UnitTestProject
 {
     [TestClass]
-    class MysqlConnectionTest
+    public class MysqlConnectionTest
     {
-
+        [TestMethod]
+        public void connectMysqlDatabase()
+        {
+            var context = new AuthDBContext();
+            context.Database.EnsureCreated();
+            var user = new User("admin","admin");
+            context.Add(user);
+        }
 
     }
 }
