900b5482cc8ab59ba46410eda88692dbfc626008
chenjianrui <chenjianrui@beisen.com>
2020-11-17 16:38:55 +0800

    新建webapi项目

---
+++ src/EFCoreApp/MyDBContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EFCoreApp
+{
+    public class MyDBContext : DbContext
+    {
+        public MyDBContext() { }
+
+        public MyDBContext(DbContextOptions<MyDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<User> User { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+    }
+}
